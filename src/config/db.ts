df1d0cf0b1757a66f910706b071b5b358786aecc
Jonathan <jonathanvanegasg@hotmail.com>
2025-09-24 17:30:18 -0500

    modified db

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,5 @@
 import { ProductsShoppingCart } from "@/features/storeGeneral/types/product.interface";
-import Dexie, { type EntityTable } from "dexie";
+import Dexie, { EntityTable } from "dexie";
 
 const db = new Dexie("market") as Dexie & {
   productShoppingCart: EntityTable<ProductsShoppingCart, "id">;
