8063af8984e1935c78eafe9d8b29978fa6290878
Jonathan <jonathanvanegasg@hotmail.com>
2025-09-24 18:07:17 -0500

    modified

--- src/config/api.ts
+++ src/config/api.ts
@@ -3,4 +3,5 @@ import { enviroments } from "./enviroments";
 export const apiURLS = {
   products: `${enviroments.apiUrl}/products`,
   categories: `${enviroments.apiUrl}/products/category-list`,
+  productsDetail: `${enviroments.apiUrlServer}/products`,
 };
--- src/features/detailProduct/service/cardDetail.service.ts
+++ src/features/detailProduct/service/cardDetail.service.ts
@@ -2,6 +2,6 @@ import { apiURLS } from "@/config/api";
 import axios from "axios";
 
 export const getProducById = async (id: string) => {
-  const response = await axios.get(`${apiURLS.products}/${id}`);
+  const response = await axios.get(`${apiURLS.productsDetail}/${id}`);
   return response;
 };
