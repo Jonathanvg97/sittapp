c2904a7b69495ddfc5b6acf108e96f69e7411254
Jonathan <jonathanvanegasg@hotmail.com>
2025-09-24 18:28:49 -0500

    debug

--- src/features/detailProduct/service/cardDetail.service.ts
+++ src/features/detailProduct/service/cardDetail.service.ts
@@ -2,6 +2,8 @@ import { apiURLS } from "@/config/api";
 import axios from "axios";
 
 export const getProducById = async (id: string) => {
+  console.log("API_URL (server):", process.env.API_URL);
+  console.log("apiURLS.productsDetail:", apiURLS.productsDetail);
   const response = await axios.get(`${apiURLS.productsDetail}/${id}`);
   return response;
 };
