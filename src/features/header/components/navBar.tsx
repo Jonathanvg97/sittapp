d308189ee238fad45a8cff8f586d181c6829c546
Jonathan <jonathanvanegasg@hotmail.com>
2025-09-24 15:32:07 -0500

    modified navbar

--- src/features/header/components/navBar.tsx
+++ src/features/header/components/navBar.tsx
@@ -2,12 +2,12 @@ import { ShoppingCartIcon } from "lucide-react";
 
 export const NavBar = () => {
   return (
-    <article className="NavBar text-black flex items-center justify-between gap-2 p-4 border-b border-gray-200">
-      <h1 className="font-bold text-xl ml-16">ShopCaralog</h1>
+    <header className="NavBar text-black flex items-center justify-between gap-2 p-4 border-b border-gray-200">
+      <h1 className="font-bold text-xl ml-4 sm:ml-16">ShopCaralog</h1>
       <ShoppingCartIcon
         size={24}
-        className="cursor-pointer mr-20 text-gray-500"
+        className="cursor-pointer mr-4 sm:mr-20 text-gray-500"
       />
-    </article>
+    </header>
   );
 };
