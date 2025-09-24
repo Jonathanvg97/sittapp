8a222362711ee9f693772adcc3123be956e404e5
Jonathan <jonathanvanegasg@hotmail.com>
2025-09-24 18:25:52 -0500

    modified

--- src/config/api.ts
+++ src/config/api.ts
@@ -3,5 +3,5 @@ import { enviroments } from "./enviroments";
 export const apiURLS = {
   products: `${enviroments.apiUrl}/products`,
   categories: `${enviroments.apiUrl}/products/category-list`,
-  productsDetail: `https://dummyjson.com/products`,
+  productsDetail: `${enviroments.apiUrlServer}/products`,
 };
--- src/config/enviroments.ts
+++ src/config/enviroments.ts
@@ -1,3 +1,4 @@
 export const enviroments = {
   apiUrl: process.env.NEXT_PUBLIC_API_URL,
+  apiUrlServer: process.env.API_URL || "https://dummyjson.com",
 };
